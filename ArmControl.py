69c303234bf559ecfa3e37eaca3073e37325d05f
Geraldine Barreto <geka_185@hotmail.com>
2020-09-30 15:31:57 -0400

    Fixed moveState error

--- ArmControl.py
+++ ArmControl.py
@@ -97,11 +97,11 @@ def LSS_IK(targetXYZG):
 
 def LSSA_moveMotors(angles_BSEWG):
 
-    # If the servos detect a current >= 600mA(gripper)/1A(other servos) before reaching the requested positions they will halt and hold
+    # If the servos detect a current >= 600mA(base and gripper)/1A(other servos) before reaching the requested positions they will halt and hold
     wrist.moveCH(angles_BSEWG[3], 1000)
     shoulder.moveCH(angles_BSEWG[1], 1000)
     elbow.moveCH(angles_BSEWG[2], 1000)
-    base.moveCH(angles_BSEWG[0], 1000)
+    base.moveCH(angles_BSEWG[0], 600)
     gripper.moveCH(angles_BSEWG[4], 600)
 
     arrived = False
@@ -117,15 +117,14 @@ def LSSA_moveMotors(angles_BSEWG):
         # If a status is None print message
         if (bStat is None or sStat is None or eStat is None or wStat is None or gStat is None):
             print("- Unknown status")
-            arrived = False
         # If the statuses aren't None check their values
         else:
             # If a servo is Outside limits, Stuck, Blocked or in Safe Mode before it reaches the requested position reset the servos and return issue
-            if (bStat>'6'  or sStat>'6' or eStat>'6'or wStat>'6' or gStat>'6'):
-                allMotors.setColorLED(lssc.LSS_LED_Red)
-                allMotors.reset
-                time.sleep(2)
-                allMotors.confirm
+            if (bStat>'6' or sStat>'6' or eStat>'6'or wStat>'6' or gStat>'6'):
+                # allMotors.setColorLED(lssc.LSS_LED_Red)
+                # allMotors.reset
+                # time.sleep(2)
+                # allMotors.confirm
                 print("- Issue detected")
                 issue = True
             # If all the servos are holding positions check if they have arrived
@@ -139,22 +138,20 @@ def LSSA_moveMotors(angles_BSEWG):
                     print("- Unknown position")
                 # If they are holding in a different position than requested one return issue
                 elif (abs(int(bPos)-angles_BSEWG[0])>20 or abs(int(sPos)-angles_BSEWG[1])>20 or abs(int(ePos)-angles_BSEWG[2])>20 or abs(int(wPos)-angles_BSEWG[3])>20):
+                    print("- Obstacle detected")
+                    issue = True
+
                     # Debugging
-                    print("Base Current (mA) = " + str(base.getCurrent()))
                     print("Shoulder Current (mA) = " + str(shoulder.getCurrent()))
                     print("Elbow Current (mA) = " + str(elbow.getCurrent()))
                     print("Wrist Current (mA) = " + str(wrist.getCurrent()))
-
-                    print("- Obstacle detected")
-                    issue = True
                 else:
                     print("- Arrived\n")
                     arrived = True
 
     return(arrived)
 
-def CBtoXY(targetCBsq, params):
-    global playerColor
+def CBtoXY(targetCBsq, params, color):
 
     wletterWeight = [-4, -3, -2, -1, 1, 2, 3, 4]
     bletterWeight = [4, 3, 2, 1, -1, -2, -3, -4]
@@ -164,7 +161,7 @@ def CBtoXY(targetCBsq, params):
         x = 6 * params["sqSize"]
         y = 6 * params["sqSize"]
     else:
-        if playerColor:         # White -> Robot color = Black
+        if color:         # White -> Robot color = Black
             sqletter = bletterWeight[ord(targetCBsq[0]) - 97]
             sqNumber = bnumberWeight[int(targetCBsq[1]) - 1]
         else:                   # Black
@@ -176,11 +173,8 @@ def CBtoXY(targetCBsq, params):
 
     return(x,y)
 
-def executeMove(move, params, color, homography, cap):
-    global playerColor
+def executeMove(move, params, color, homography, cap, selectedCam):
 
-    moveState = False
-    playerColor = color
     allMotors.setColorLED(lssc.LSS_LED_Cyan)
     z = params["cbHeight"] + params["pieceHeight"]
     angles_rest = (0,-1100,450,1100,0)
@@ -192,17 +186,17 @@ def executeMove(move, params, color, homography, cap):
     for i in range(0,len(move),2):
 
         # Move to position (up)
-        x, y = CBtoXY((move[i],move[i+1]), params)
+        x, y = CBtoXY((move[i],move[i+1]), params, color)
         angles_BSEWG1 = LSS_IK([x, y, z + 1, gripState])
         print("1) MOVE UP")
         arrived1 = LSSA_moveMotors(angles_BSEWG1)
-        askPermision(angles_BSEWG1, arrived1, homography, cap)
+        askPermision(angles_BSEWG1, arrived1, homography, cap, selectedCam)
 
         # Go down
         angles_BSEWG2 = LSS_IK([x, y, z - 1 - goDown, gripState])
         print("2) GO DOWN")
         arrived2 = LSSA_moveMotors(angles_BSEWG2)
-        askPermision(angles_BSEWG2, arrived2, homography, cap)
+        askPermision(angles_BSEWG2, arrived2, homography, cap, selectedCam)
 
         if (i/2)%2: # Uneven move (go lower to grab the piece)
             gripState = gOpen
@@ -220,18 +214,17 @@ def executeMove(move, params, color, homography, cap):
         angles_BSEWG3 = LSS_IK([x, y, z + 1, gripState])
         print("4) GO UP")
         arrived3 = LSSA_moveMotors(angles_BSEWG3)
-        askPermision(angles_BSEWG3, arrived3, homography, cap)
+        askPermision(angles_BSEWG3, arrived3, homography, cap, selectedCam)
 
     # Go back to resting position and go limp
     print("5) REST")
-    _ = LSSA_moveMotors(angles_rest)
-    time.sleep(2)
+    moveState = LSSA_moveMotors(angles_rest)
     allMotors.limp()
     allMotors.setColorLED(lssc.LSS_LED_Black)
 
     return(moveState)
 
-def askPermision(angles_BSEWG, arrived, homography, cap):
+def askPermision(angles_BSEWG, arrived, homography, cap, selectedCam):
 
     angles_rest = (0,-1100,450,1100,0)
     sec = 0
@@ -249,7 +242,7 @@ def askPermision(angles_BSEWG, arrived, homography, cap):
         sec = 0
 
         while arrived == False and sec < 5:                 # If the servos couldn't reach the requested position and we haven't waited 5 sec
-            if vm.safetoMove(homography, cap) != 0 or sec == 4:  # We check if it is safe to move (vision code) or if we have waited 5 sec
+            if vm.safetoMove(homography, cap, selectedCam) != 0 or sec == 4:  # Check if it is safe to move (vision code) or if we have waited 5 sec
                 allMotors.setColorLED(lssc.LSS_LED_Cyan)    # If it is true we change LEDs back to cyan
                 arrived = LSSA_moveMotors(angles_BSEWG)     # And try moving the motors again
             else:
